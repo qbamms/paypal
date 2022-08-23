916f914f7925eb324af15e45e4bc21f4f4f7de82
wizzy <wizzy@gmail.com>
2022-08-23 13:44:18 +0000

    Updated Bash Shell Script for IF syntax file

--- if.sh
+++ if.sh
@@ -2,7 +2,7 @@
 #This is a bash shell for if syntax
 echo "Please enter the amount you are willing to pay"
 read amount
-if (($amount -eq 500 )) 
+if [ $amount == 500 ] 
 then
 	echo "transaction approved"
 else
