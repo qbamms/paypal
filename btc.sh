8d71bc88d58882d701ea8add6fcc48170e0a6056
wizzy <wizzy@gmail.com>
2022-08-23 13:25:58 +0000

    Bitcoing Shell script

---
+++ btc.sh
@@ -0,0 +1,7 @@
+#/!/bin/bash
+#This file is a bash shell script for bitcoin trading
+echo "Please enter your username"
+read username
+echo "please enter your password"
+read password -s 
+echo "You have successfully login"
